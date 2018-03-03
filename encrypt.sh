506e1656292704a49350c865120f5f7279c26799
Felipe Corniani de Genaro <felipegenaro@MacBook-Air.local>
2018-03-03 19:28:11 -0300

    openssl command for encrypt files

---
+++ encrypt.sh
@@ -0,0 +1,5 @@
+# Advanced Encryption Standard
+
+openssl enc -aes-256-cbc -e -in /Users/felipegenaro/Desktop/fileToEncrypt.exten -out /Users/felipegenaro/Desktop/fileEncrypted.exten
+
+openssl enc -aes-256-cbc -d -in /Users/felipegenaro/Desktop/fileToDencrypt.exten -out /Users/felipegenaro/Desktop/fileDencrypted.exten
